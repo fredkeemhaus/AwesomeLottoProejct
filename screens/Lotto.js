204570881c773b07e0d701aa7526d89387462d9a
fredkeemhaus <fredkeemhaus@gmail.com>
2020-04-30 14:34:36 +0900

    lotto number return

--- screens/Lotto.js
+++ screens/Lotto.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { item } from "../api";
 import moment from "moment";
 import styled from "styled-components";
+import _ from 'lodash';
 
 const NowLottoNumberContainer = styled.View`
   flex-direction: row;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
 `;
 
@@ -17,6 +18,17 @@ const LottoCircleBox = styled.View`
   align-items: center;
 `;
 
+const LottoNumberCircleBox = styled.View`
+  width: 30px;
+  height: 30px;
+  background-color: red;
+  justify-content: center;
+  align-items: center;
+  border-radius: 30px;
+`
+
+// background-color: ${props => (props.backgroundCondition ? 'white' : 'transparent')};
+
 const nowDate = new Date();
 const momentNowDate = moment(nowDate).format("YYYY. MM .DD");
 
@@ -24,7 +36,8 @@ export default class Lotto extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      drwNumberData: null
+      drwNumberData: null,
+      lottoNumber: null
     };
   }
 
@@ -50,29 +63,101 @@ export default class Lotto extends Component {
     }
   };
 
-  render() {
-    const { drwNumberData } = this.state;
+  _numberReturn = () => {
+    let lottoNumber = _.sampleSize(_.range(1, 45), 7)
+    this.setState({
+      lottoNumber
+    })
+  }
 
+  render() {
+    const { drwNumberData, lottoNumber } = this.state;
+    console.log(lottoNumber, '-')
     return (
-      <View>
-        <View>
-          <Text>현재 날짜 {momentNowDate}</Text>
-          <View>
-            <Text>{drwNumberData && drwNumberData.drwNo} 회차</Text>
-            <Text>{drwNumberData && drwNumberData.drwNo} 회차</Text>
+      <View style={{paddingHorizontal: 30}}>
+        <View style={{marginTop: 20}}>
+          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+            <Text>현재 날짜</Text>
+            <Text>{momentNowDate}</Text>
           </View>
-          <NowLottoNumberContainer>
-            <LottoCircleBox>
-              <Text>{drwNumberData && drwNumberData.drwtNo1}</Text>
-            </LottoCircleBox>
-            <Text>{drwNumberData && drwNumberData.drwtNo2}</Text>
-            <Text>{drwNumberData && drwNumberData.drwtNo3}</Text>
-            <Text>{drwNumberData && drwNumberData.drwtNo4}</Text>
-            <Text>{drwNumberData && drwNumberData.drwtNo5}</Text>
-            <Text>{drwNumberData && drwNumberData.drwtNo6}</Text>
-            <Text>{drwNumberData && drwNumberData.drwtNo7}</Text>
-          </NowLottoNumberContainer>
+          <View style={{backgroundColor: 'white', padding: 20, borderRadius: 14, marginVertical: 10}}>
+            <View style={{width: '100%', flexDirection: 'row', justifyContent: 'flex-end', marginBottom: 10}}>
+              <Text>{drwNumberData && drwNumberData.drwNo} 회차</Text>
+            </View>
+            <NowLottoNumberContainer>
+              <LottoNumberCircleBox>
+                  <Text>
+                    {drwNumberData && drwNumberData.drwtNo1}
+                  </Text>
+              </LottoNumberCircleBox>
+              <LottoNumberCircleBox>
+                  <Text>
+                    {drwNumberData && drwNumberData.drwtNo2}
+                  </Text>
+              </LottoNumberCircleBox>
+              <LottoNumberCircleBox>
+                  <Text>
+                    {drwNumberData && drwNumberData.drwtNo3}
+                  </Text>
+              </LottoNumberCircleBox>
+              <LottoNumberCircleBox>
+                  <Text>
+                    {drwNumberData && drwNumberData.drwtNo4}
+                  </Text>
+              </LottoNumberCircleBox>
+              <LottoNumberCircleBox>
+                  <Text>
+                    {drwNumberData && drwNumberData.drwtNo5}
+                  </Text>
+              </LottoNumberCircleBox>
+              <LottoNumberCircleBox>
+                  <Text>
+                    {drwNumberData && drwNumberData.drwtNo6}
+                  </Text>
+              </LottoNumberCircleBox>
+              <LottoNumberCircleBox>
+                  <Text>
+                    {drwNumberData && drwNumberData.bnusNo}
+                  </Text>
+              </LottoNumberCircleBox>
+            </NowLottoNumberContainer>
+          </View>
         </View>
+        {lottoNumber ? (
+          <>
+            <NowLottoNumberContainer>
+              <LottoNumberCircleBox>
+                <Text>{lottoNumber[0]}</Text>
+              </LottoNumberCircleBox>
+              <LottoNumberCircleBox>
+                <Text>{lottoNumber[1]}</Text>
+              </LottoNumberCircleBox>
+              <LottoNumberCircleBox>
+                <Text>{lottoNumber[2]}</Text>
+              </LottoNumberCircleBox>
+              <LottoNumberCircleBox>
+                <Text>{lottoNumber[3]}</Text>
+              </LottoNumberCircleBox>
+              <LottoNumberCircleBox>
+                <Text>{lottoNumber[4]}</Text>
+              </LottoNumberCircleBox>
+              <LottoNumberCircleBox>
+                <Text>{lottoNumber[5]}</Text>
+              </LottoNumberCircleBox>
+              <LottoNumberCircleBox>
+                <Text>{lottoNumber[6]}</Text>
+              </LottoNumberCircleBox>
+            </NowLottoNumberContainer>
+          </>
+        ) : (
+          <TouchableOpacity
+            onPress={this._numberReturn}
+          >
+            <Text>
+              번호 뽑기
+            </Text>
+          </TouchableOpacity>
+        )}
       </View>
     );
   }
--- screens/MyPage.js
+++ screens/MyPage.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { View, Text } from "react-native";
+import {WebView} from 'react-native-webview'
 
 export default () => (
-  <View>
-    <Text>My</Text>
-  </View>
+  <WebView
+        source={{ uri: 'https://m.dhlottery.co.kr/gameResult.do?method=byWin&drwNo=908&wiselog=M_A_1_1'}}
+      />
 );
